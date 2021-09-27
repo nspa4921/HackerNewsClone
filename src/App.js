61c2237e870c41cc2a3e4380a26434bf7116b8cb
Nemanja <nemo.spaske@Live.com>
2021-09-27 14:27:39 +0200

    install axios bootstrap node-sass react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <h2>Coding Assignment</h2>
+      <Header/>
     </div>
   );
 }
